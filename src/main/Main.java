7cb02528bd460627e2d9eba3b8c2271225bed6dc
iremarpag <73463510+iremarpag@users.noreply.github.com>
2020-11-04 23:33:28 +0300

    Update Main.java
    udpServer adding main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,6 +8,7 @@ public class Main {
 //        Server server = new Server(Server.DEFAULT_SERVER_PORT);
 //        API DENEMEK İÇİN SERVER KURULUMU COMMENTLEDIM
         ConnectionOpenWeatherMap connection = new ConnectionOpenWeatherMap(ConnectionOpenWeatherMap.DEFAULT_BASE_URL, ConnectionOpenWeatherMap.DEFAULT_CONTENT);
+        udp_server.udpServer();
         System.out.println(connection.buildConnection("london"));
     }
 }
